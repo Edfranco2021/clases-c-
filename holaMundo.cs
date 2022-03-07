d5ce9b0a4cecd269c25a5f25cbd309fc4f75fb1a
edfranco2021 <edgardofranco@gmail.com>
2022-03-07 17:45:59 -0300

    primer programa

---
+++ holaMundo.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System.Security.AccessControl;
+using System;
+using FileSystemAclExtensions;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo, prueba en C");
+        }
+    }
+}
+
